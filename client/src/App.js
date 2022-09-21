a3b76e2afa1aca5cf0ec6bb114a561749b54fc99
doblealberto <97196493+doblealberto@users.noreply.github.com>
2022-09-21 15:04:46 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Fragment>
         <header className="header">
-          <div>This is a multicontainer application</div>
+          <div>This is no5 a multicontainer application</div>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other page</Link>
         </header>
